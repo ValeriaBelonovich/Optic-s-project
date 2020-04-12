558bd359fa5886024d4972aef1ba39221b3480cd
Valeria <lbelonovich@gmail.com>
2020-04-12 23:24:23 +0300

    ...

--- Optic's project/Lense.cpp
+++ Optic's project/Lense.cpp
@@ -1,25 +1,103 @@
 ﻿#include "Optics.h"
-#include <utility>
+#include <valarray>
+#include <cmath>
+#include <boost/math/special_functions/sign.hpp>
 
 class Lense
 {
 public:
-	std::pair<double, double> intersection_in(Ray& ray) // пересечение внутрь линзы
-	{}
-
+	
 	Ray refraction_in(Ray & ray) // прломление внутрь линзы
-	{}
-
-	std::pair<double, double> intersection_out(Ray& ray) // пересечение из линзы
 	{}
 
 	Ray refraction_out(Ray & ray) // преломление из линзы
 	{}
 
 private:
-	std::pair<double,double> centre; //координаты центра
+	std::valarray<double> intersection(Ray& ray, double r);//ищем пересечние луча со сферической поверхностью заданного радиуса кривизны
+	std::valarray<double> refraction(Ray& ray, double r);
+	std::valarray<double> slu();
+
+private:
+	std::valarray<double>[3] centre; //координаты центра
 	double length; //длина
 	double n; // показатель преломления
 	double r_left; //левый радиус кривизны
-	double r_rightж // правый радиус кривизны
+	double r_right // правый радиус кривизны
 };
+
+std::valarray<double> Lense::intersection(Ray& ray, double r) //ищем пересечние луча со сферической поверхностью заданного радиуса кривизны
+{
+	std::valarray<double>[3] result;
+	double centre_curve = centre[0] + boost::math::sign(r)*sqrt(pow(r, 2) + pow(length / 2, 2)); //центр  кривизны поверхности
+	a = pow(ray.x, 2) + pow(ray.y, 2) + pow(ray.z, 2); // коэффициенты в получающемся квадратном уравнении
+	b = 2 * (ray.x * (ray.begin[0] - centre_curve) + ray.y * ray.begin[1] + ray.z * ray.begin[2]);
+	c = pow(ray.begin[0], 2) + pow(ray.begin[1], 2) + pow(ray.begin[2], 2) + pow(centre_curve, 2) - pow(r, 2);
+
+	discriminant = pow(b, 2) - 4 * a * c;
+
+	if (discriminant > 0)
+	{
+		if (r > 0) // если нужная нам часть вылуклая вправо
+		{
+			t = (-b + sqrt(discriminant)) / (2 * a);
+			for (int i = 0; i < 3; ++i)
+			{
+				result[i] = ray.begin[i] + t * ray.cos[i];
+			}
+		}
+		else //нужна часть сферы выпуклая влево
+		{
+			t = (-b - sqrt(discriminant)) / (2 * a);
+			for (int i = 0; i < 3; ++i)
+			{
+				result[i] = ray.begin[i] + t * ray.cos[i];
+			}
+		}
+
+	}
+	else // если луч не соприкасается со сферой или идет по касательной - он не меняется
+	{
+		return ray
+	}
+	return result;
+}
+
+std::array<double>[3] Lense::refraction(Ray& ray, double r)
+{
+	result.begin = intersection(ray, r);
+	std::array<double>[3] normal; //наверное потом унесу все в  функции операций для векторов
+	normal[0] = centre[0] + boost::math::sign(r) * sqrt(pow(r, 2) + pow(length / 2, 2)) - result.begin[0]; 
+	normal[1] = -result.begin[1];
+	normal[2] = -result.begin[2];
+
+	double sin_alpha = sqrt(1 - pow( (normal[0] * ray.cos[0] + normal[1] * ray.cos[1] + normal[2] * ray.cos[2]) / 
+	( ( pow(normal[0],2) + pow(normal[1], 2) + pow(normal[2], 2) )*(pow(ray.cos[0], 2) + pow(ray.cos[1], 2) pow(ray.cos[2], 2), 2) ));
+
+	double alpha = std::asin(sin_alpha);
+
+	double sin_beta = sin_alpha / n; //закон преломления еее
+
+	double beta = std::asin(sin_beta);
+
+
+	//в итоге чтобы определить координаты преломившегося луча надо решить слу учитывающую компланарность падающего, 
+	//преломленного лучей и нормали, угол паддающего с нормалью, угол преломленного с падающим
+	
+
+}
+
+std::array<double>[3] slu(std::array<double>[3] & normal, std::array<double>[3] & fall, double alpha, double beta)
+{
+
+}
+
+
+
+Ray Lense::refraction_in(Ray& ray)
+{
+	Ray result;
+
+	result.begin = intersection(ray, r_left);
+	
+}
--- Optic's project/Ray.cpp
+++ Optic's project/Ray.cpp
@@ -7,15 +7,11 @@ public:
 	void send()//отправить луч на отрисовку (как только заполняется end)
 	{}
 
-	auto equation()//вывод уравнения прямой которой задается луч, не уверенна в типе
-	{}
-
 	auto coordinates()// вывод координат для расчета аберраций, не уверенна в типе
 	{}
 
-private:
-	std::pair<double, double> begin; //начало луча
-	std::pair<double, double> end; //конец луча
-	double a;
-	double b;// направляющие косинусы
+//private:
+	std::array<int>[3] begin; //начало луча
+	std::array<int>[3] end; //конец луча
+	std::array<int>[3] cos// направляющие косинусы
 };
