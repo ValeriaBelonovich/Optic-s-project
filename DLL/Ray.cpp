26f2e6900d14d2bd50f59fdc6482b9e224ef1a76
ValeriaBelonovich <61022359+ValeriaBelonovich@users.noreply.github.com>
2020-10-21 19:36:33 +0300

    Add files via upload

---
+++ DLL/Ray.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "OpticsClasses.hpp"
+
+
+namespace Opticsproject
+{
+	void Ray::send(json& r)
+	{
+		json temp;
+		for (int i = 0; i < dim; ++i)
+		{
+			temp["begin"].push_back(begin[i]);
+			temp["end"].push_back(end[i]);
+		}
+
+		temp["rgb"] = rgb;
+
+		r.push_back(temp);
+		return; 
+	}
+}
